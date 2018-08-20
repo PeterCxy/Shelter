eab994ea4cfcaa764e9687830319a7096c370001
Peter Cai <peter@typeblog.net>
2018-08-20 16:38:07 +0800

    MainActivity: remove unused imports

--- app/src/main/java/net/typeblog/shelter/ui/MainActivity.java
+++ app/src/main/java/net/typeblog/shelter/ui/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
